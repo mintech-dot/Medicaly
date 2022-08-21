54535c54d09a41d8c6883edb96edaa63d89d18f9
Sobhi Raid <75437079+mintech-dot@users.noreply.github.com>
2022-08-21 23:42:01 +0100

    Update done.blade.php

--- resources/views/done.blade.php
+++ resources/views/done.blade.php
@@ -10,13 +10,13 @@
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
     </div>
     <div class="lg:w-1/2 md:w-2/3 mx-auto">
-    <form action="/joinus/store" method="post">
+    <form action="/orders/store" method="post">
       @csrf
       <div class="flex flex-wrap -m-2">
         <div class="p-2 w-1/2">
           <div class="relative">
             <label for="name" class="leading-7 text-sm text-gray-600">Full name</label>
-            <input type="text" name="fname" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+            <input type="text"  name="fname" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
           </div>
         </div>
         <div class="p-2 w-1/2">
@@ -29,32 +29,27 @@
         <div class="p-2 w-1/2">
           <div class="relative">
             <label for="name" class="leading-7 text-sm text-gray-600">Phone Number</label>
-            <input type="text" name="pnumber" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+            <input type="text"  name="pnumber" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
           </div>
         </div>
         <div class="p-2 w-1/2">
           <div class="relative">
-            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+            <label for="email" class="leading-7 text-sm text-gray-600">Clinic Name</label>
+            <input type="email"  name="med" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
           </div>
         </div>
 
-        <div class="p-2 w-full">
+        <div class="p-2 w-1/2">
           <div class="relative">
-            <label for="name" class="leading-7 text-sm text-gray-600">Worktime</label>
-            <input type="text" name="worktime" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+            <label for="name" class="leading-7 text-sm text-gray-600">Appointement Day</label>
+            <input type="text"  name="date" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
           </div>
         </div>
         
-
         <div class="p-2 w-full">
-          <div class="relative">
-            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-            <textarea  name="description" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required></textarea>
-          </div>
-        </div>
-        <div class="p-2 w-full">
+          <a href="/done" >
           <button type="submit" class="flex mx-auto text-white bg-[#ff748e] border-0 py-2 px-8 focus:outline-none hover:bg-[#fc8da1] rounded text-lg">Submit</button>
+          </a>
         </div>
       </div>
       </form>
