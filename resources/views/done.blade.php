661c376d714838327300cae5d7e1d86e31c53d0b
Sobhi Raid <75437079+mintech-dot@users.noreply.github.com>
2022-08-21 23:41:44 +0100

    Update done.blade.php

--- resources/views/done.blade.php
+++ resources/views/done.blade.php
@@ -1,27 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Done !</title>
-</head>
-    <body >
-        <section class="text-gray-800 body-font">
-            <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-              <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" src="{{ URL('img/order.svg') }}">
-              <div class="text-center lg:w-2/3 w-full">
-                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium bg-clip-text text-transparent bg-[#044D5E]">Booking request sent successfully</h1>
-                <p class="mb-8 leading-relaxed">Your request will be confirmed by one of the clinic workers</p>
-                <div class="flex justify-center">
-                  <a href="/" class="inline-flex" >
-                  <button class=" text-white bg-[#ff748e] border-0 py-2 px-6 focus:outline-none hover:bg-[#fc8da1] rounded-full text-lg">Back to Home</button>
-                  <a>
-                </div>
-                
-              </div>
-            </div>
-          </section>
-    </body>
-</html>
+@extends('layout')
+
+@section('content')
+    
+
+<section class="text-gray-600 body-font relative">
+  <div class="container px-5 py-24 mx-auto border-width: 1px bg-white shadow-xl rounded pt-6 pb-8">
+    <div class="flex flex-col text-center w-full mb-12 ">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+    </div>
+    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+    <form action="/joinus/store" method="post">
+      @csrf
+      <div class="flex flex-wrap -m-2">
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="name" class="leading-7 text-sm text-gray-600">Full name</label>
+            <input type="text" name="fname" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+          </div>
+        </div>
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="email" class="leading-7 text-sm text-gray-600">Address</label>
+            <input type="email"  name="address" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+          </div>
+        </div>
+
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="name" class="leading-7 text-sm text-gray-600">Phone Number</label>
+            <input type="text" name="pnumber" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+          </div>
+        </div>
+        <div class="p-2 w-1/2">
+          <div class="relative">
+            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+            <input type="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+          </div>
+        </div>
+
+        <div class="p-2 w-full">
+          <div class="relative">
+            <label for="name" class="leading-7 text-sm text-gray-600">Worktime</label>
+            <input type="text" name="worktime" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+          </div>
+        </div>
+        
+
+        <div class="p-2 w-full">
+          <div class="relative">
+            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+            <textarea  name="description" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-[#044D5E] focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required></textarea>
+          </div>
+        </div>
+        <div class="p-2 w-full">
+          <button type="submit" class="flex mx-auto text-white bg-[#ff748e] border-0 py-2 px-8 focus:outline-none hover:bg-[#fc8da1] rounded text-lg">Submit</button>
+        </div>
+      </div>
+      </form>
+
+    </div>
+  </div>
+</section>
+    
+
+
+
+  
+@endsection
