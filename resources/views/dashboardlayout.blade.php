d0786685865f3f06edf0d7119c0772e3cd67c0ef
Sobhi Raid <75437079+mintech-dot@users.noreply.github.com>
2022-08-29 21:24:32 +0100

    Update dashboardlayout.blade.php

--- resources/views/dashboardlayout.blade.php
+++ resources/views/dashboardlayout.blade.php
@@ -126,7 +126,7 @@
               
               
               
-                  <!-- jQuery -->
+     <!-- jQuery -->
      <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
      <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
      <script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
