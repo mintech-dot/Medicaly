bbe3228db73f36d9f3ba5efb2337f3f1b3dd0cd8
Sobhi Raid <75437079+mintech-dot@users.noreply.github.com>
2022-08-31 19:43:25 +0100

    Update joinus.blade.php

--- resources/views/joinus.blade.php
+++ resources/views/joinus.blade.php
@@ -6,8 +6,7 @@
 <section class="text-gray-600 body-font relative">
   <div class="container px-5 py-24 mx-auto border-width: 1px bg-white shadow-xl rounded pt-6 pb-8">
     <div class="flex flex-col text-center w-full mb-12 ">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Help Us</h1>
     </div>
     <div class="lg:w-1/2 md:w-2/3 mx-auto">
     <form action="/joinus/store" method="post">
